ea31d98448f7e159a4f271c056428c900598ad72
ziwei wu <ziweiwu@gmail.com>
2016-08-11 16:54:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+SICP is a book that recommended by many as one of the best book to learn programming.
+
+I hope I can keep track of my progress learning this book with this repo.
+
+
+
+  
---
+++ chapter-1/define-pi.scm
@@ -0,0 +1,9 @@
+;define pi and radius   
+(define pi 3.14)
+(define radius 10)
+(* pi ( * radius radius))
+;define a circumference function using already defined functions
+(define circumference ( * pi radius ))
+;output the result
+circumference
+
